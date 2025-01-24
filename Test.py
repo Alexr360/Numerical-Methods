822ea6e92923b42bd8db54f92aa91f26e0dd4438
Alexr360 <alexr000360@gmail.com>
2025-01-24 13:40:36 -0700

    Updated test program to be random insult program

--- Test.py
+++ Test.py
@@ -1,1 +1,5 @@
-print("Hello, World!")
+import random
+
+name = input("Whats your name?\nA")
+list_of_insults = ["You are a bastard", "You suck", "You should actually teach us numerical methods", "Where is my numerical methods", "This doesen't look like numerical methods to me", "Im not hear to learn python", "Please actually teach something useful"]
+print(str(random.choice(list_of_insults)) + " " + name + ".")
