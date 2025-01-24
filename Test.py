0c627575f0c7e8e50a7bbb6281ff664608c54ed4
Alexr360 <alexr000360@gmail.com>
2025-01-24 13:48:56 -0700

    Added insults only to test.py

--- Test.py
+++ Test.py
@@ -1,5 +1,9 @@
 import random
 
 name = input("Whats your name?\n")
-list_of_insults = ["You are a bastard", "You suck", "You should actually teach us numerical methods", "Where is my numerical methods", "This doesen't look like numerical methods to me", "Im not hear to learn python", "Please actually teach something useful"]
-print(str(random.choice(list_of_insults)) + " " + name + ".")
+if "tie" in name.lower() or "wei" in name.lower():
+    list_of_insults = ["You should actually teach us numerical methods", "Where is my numerical methods", "This doesen't look like numerical methods to me", "Im not hear to learn python", "Please actually teach something useful"]
+    print(str(random.choice(list_of_insults)) + " " + name + ".")
+else:
+    list_of_platitudes = ["How nice you are", "Nice to meet you", "Goodbye"]
+    print(str(random.choice(list_of_platitudes)) + " " + name + ".")
