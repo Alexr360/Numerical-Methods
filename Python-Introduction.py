3301d3a482dd0394fef949fb2b851564e62521ca
Alexr360 <alexr000360@gmail.com>
2025-01-27 13:04:01 -0700

    Added Comments

--- Python-Introduction.py
+++ Python-Introduction.py
@@ -1,3 +1,13 @@
-for i in range(100):
-  # Code to be executed 10 times
-  print("Hello, World!") 
+import random  # Importing the 'random' module to enable random selection from a list
+
+# Prompt the user to input their name and store it in the variable 'name'
+name = input("What's your name?\n")
+
+# Create a lisAlet of polite or friendly phrases
+list_of_platitudes = [
+    "How nice you are",
+    "Nice to meet you",
+    "Good Morning"
+]
+# Randomly choose one of the phrases from the list and print it with the user's name
+print(str(random.choice(list_of_platitudes)) + " " + name + ".")
--- Test.py
+++ Test.py
@@ -1,9 +1,26 @@
-import random
+import random  # Importing the 'random' module to enable random selection from a list
 
-name = input("Whats your name?\n")
+# Prompt the user to input their name and store it in the variable 'name'
+name = input("What's your name?\n")
+
+# Check if the lowercase version of the inputted name contains the substring "tie" or "wei"
 if "tie" in name.lower() or "wei" in name.lower():
-    list_of_insults = ["You should actually teach us numerical methods", "Where is my numerical methods", "This doesen't look like numerical methods to me", "Im not hear to learn python", "Please actually teach something useful"]
+    # Create a list of humorous or sarcastic comments about teaching numerical methods
+    list_of_insults = [
+        "You should actually teach us numerical methods",
+        "Where is my numerical methods",
+        "This doesn't look like numerical methods to me",
+        "I'm not here to learn Python",
+        "Please actually teach something useful"
+    ]
+    # Randomly choose one of the comments from the list and print it with the user's name
     print(str(random.choice(list_of_insults)) + " " + name + ".")
 else:
-    list_of_platitudes = ["How nice you are", "Nice to meet you", "Goodbye"]
+    # Create a list of polite or friendly phrases
+    list_of_platitudes = [
+        "How nice you are",
+        "Nice to meet you",
+        "Goodbye"
+    ]
+    # Randomly choose one of the phrases from the list and print it with the user's name
     print(str(random.choice(list_of_platitudes)) + " " + name + ".")
