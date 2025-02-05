36940e1c5f48a174f953ff9e8f1fe9c265266280
Alexr360 <alexr000360@gmail.com>
2025-02-05 13:08:54 -0700

    Updated

---
+++ sphere.py
@@ -0,0 +1,9 @@
+import math
+
+r = 5
+
+# Calculate surface area and volume
+surface_area = 4 * math.pi * r**2
+volume = 4 / 3  * math.pi * r**3
+
+print(f"Surface Area:{surface_area}\nVolume:{volume}")
