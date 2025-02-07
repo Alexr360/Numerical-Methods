93de56b95194ed8d32b82c05f3e6accc42f7ae5b
Alexr360 <alexr000360@gmail.com>
2025-02-07 13:48:20 -0700

    Updated to accept any array

--- stupidity.py
+++ stupidity.py
@@ -1,4 +1,9 @@
-colors = ['red', 'blue', 'green']
-print(type(colors))
-print(colors)
-print(colors[0:3])
+def numbers(list1):
+    one = sum(r for r in list1)
+    two = sum(r**2 for r in list1)
+    thr = sum(r**3 for r in list1)
+    return one, two, thr
+
+sutpid = numbers([1,5,3,7,8,9,5,5,5,5,5,5,5])
+
+print(f"Thing1: {sutpid[0]}, Thing2: {sutpid[1]}, Thing3: {sutpid[2]}")
