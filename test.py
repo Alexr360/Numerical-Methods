cc68c922a6f7a68050c0c9ce762f98cb57f21fb3
Alexr360 <alexr000360@gmail.com>
2025-02-19 13:49:35 -0700

    Random 3d Plat

--- test.py
+++ test.py
@@ -1,22 +1,26 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-A = np.array([0,1,2,3])
-M=np.array(
-    [
-        [
-            [
-                [1,2,3],   
-                [4,5,6]
-            ],
 
-            [
-                [11,22,33],
-                [44,55,66]
-            ]            
-        ]
-    ]
-)
+# Create data
+x = np.random.rand(100)
+y = np.random.rand(100)
+z = np.random.rand(100)
 
+# Create figure and axes object
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
-print(M)
-print(M[1][1][1])
+# Plot the data
+ax.scatter(x, y, z)
 
+# Set labels
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+# Set title
+ax.set_title('3D Scatter Plot Example')
+
+# Show the plot
+plt.show()
