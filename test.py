97fbb5cc990dc0fcc062a907ccd3894577381e8d
Alexr360 <alexr000360@gmail.com>
2025-02-19 13:51:31 -0700

    Random 3d plot

--- test.py
+++ test.py
@@ -7,12 +7,17 @@ x = np.random.rand(100)
 y = np.random.rand(100)
 z = np.random.rand(100)
 
+x1 = np.random.rand(100)
+y1 = np.random.rand(100)
+z1 = np.random.rand(100)
+
 # Create figure and axes object
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
 # Plot the data
 ax.scatter(x, y, z)
+ax.scatter(x1, y1, z1)
 
 # Set labels
 ax.set_xlabel('X Label')
