e2cb64d674428866458775d9c54ed0f6aaeef226
Alexr360 <alexr000360@gmail.com>
2025-02-26 13:35:30 -0700

    chore: Refactor code for plotting laminar pipe flow with multiple input values

--- Week 6/Laminar Pipe Flow.py
+++ Week 6/Laminar Pipe Flow.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
 def plot_laminar_pipe_flow(delta_p, mu, L, R):
@@ -14,17 +14,18 @@ input_values_list = [
     [250, 0.001, 1.0, 0.05]
 ]
 
-# Create a single plot
-plt.figure(figsize=(8, 5))
-plt.xlabel('Radial Position (m)')
-plt.ylabel('Velocity (m/s)')
-plt.title('Velocity Profile in a Laminar Pipe Flow (Hagen-Poiseuille)')
-plt.grid()
+fig = plt.figure(figsize=(8, 5))
+ax = fig.add_subplot(111, projection='3d')
 
-# Plot for each set of inputs
 for input_values in input_values_list:
     r, u = plot_laminar_pipe_flow(*input_values)
-    plt.plot(r, u, label=f'delta_p={input_values[0]}, mu={input_values[1]}, L={input_values[2]}, R={input_values[3]}')
+    R = input_values[3]
+    ax.plot(r, [R] * len(r), u, label=f'delta_p={input_values[0]}, mu={input_values[1]}, L={input_values[2]}, R={input_values[3]}')
 
-plt.legend()
+ax.set_xlabel('Radial Position (m)')
+ax.set_ylabel('R (m)')
+ax.set_zlabel('Velocity (m/s)')
+ax.set_title('Velocity Profile in a Laminar Pipe Flow (Hagen-Poiseuille)')
+ax.legend()
+
 plt.show()
