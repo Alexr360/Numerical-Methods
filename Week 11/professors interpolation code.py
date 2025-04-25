53f2a26698d7b2852815ccf49e14143bf3e6821f
Alexr360 <alexr000360@gmail.com>
2025-04-25 13:09:50 -0600

    refactor: Improve professors interpolation code for better performance and readability

--- Week 11/professors interpolation code.py
+++ Week 11/professors interpolation code.py
@@ -1,22 +1,29 @@
-import matplotlib.pyplot as plt
 import numpy as np
 
-x = [1, 2, 4, 5]  # Example x values
-y = [2, 4, 8, 10]  # Example y values
+import matplotlib.pyplot as plt
 
-xinterp = np.linspace(x[0], x[-1], 100)  # Interpolation points
-yinterp = 0
+def interpolate(x_query, data_points):
+    x = [point[0] for point in data_points]
+    y = [point[1] for point in data_points]
 
-for i in range(len(x)):
-    Li = 1
-    for j in range(len(x)):
-        if i != j:
-            Li *= (xinterp - x[j]) / (x[i] - x[j])
-    yinterp += Li * y[i]
+    xinterp = np.linspace(x[0], x[-1], 100)  # Interpolation points
+    yinterp = 0
 
-plt.plot(x, y, 'o-', label='Data Points')
-plt.plot(xinterp, yinterp, 'r-', label='Interpolated Point')
-plt.xlabel('x')
-plt.ylabel('y')
-plt.legend()
-plt.show()
+    for i in range(len(x)):
+        Li = 1
+        for j in range(len(x)):
+            if i != j:
+                Li *= (xinterp - x[j]) / (x[i] - x[j])
+        yinterp += Li * y[i]
+
+    plt.plot(x, y, 'o-', label='Data Points')
+    plt.plot(xinterp, yinterp, 'r-', label='Interpolated Point')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.legend()
+    plt.show()
+
+# Example usage
+x_query = 3.5
+data_points = [(1, 2), (2, 4), (4, 8), (5, 10)]
+interpolate(x_query, data_points)
