9199a5dd6f3d3297b0ccd7a3a095ace00134e233
Alexr360 <alexr000360@gmail.com>
2025-04-25 13:44:20 -0600

    refactor: Improve Finite Difference method for numerical differentiation

--- Week 12/Finite Difference.py
+++ Week 12/Finite Difference.py
@@ -4,10 +4,16 @@ import matplotlib.pyplot as plt
 
 # Define the function f(x)
 def f(x):
+    """
+    Returns the value of the function f(x) = 2x^3 - 5x^2 + 3x + 1.
+    """
     return 2*x**3 - 5*x**2 + 3*x + 1
 
 # Define the derivative of the function f'(x)
 def f_prime(x):
+    """
+    Returns the value of the derivative of the function f'(x) = 6x^2 - 10x + 3.
+    """
     return 6*x**2 - 10*x + 3
 
 # Generate x and y values for plotting
@@ -15,6 +21,7 @@ x = np.linspace(0, 2, 50)
 y = f(x)
 y_prime = f_prime(x)
 
+# Define forward and backward difference approximations
 forward_difference = lambda x, h: (f(x + h) - f(x)) / h
 backward_difference = lambda x, h: (f(x) - f(x - h)) / h
 
